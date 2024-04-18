eeba91f84d0382f26fae845f80be655c3584f745
chukez <39062768+chukez@users.noreply.github.com>
2024-04-18 10:52:11 +0100

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -3,14 +3,18 @@
 pragma solidity ^0.8.20;
 
 contract contractA {
-    address real_owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address real_owner = 0x9581a8b08621139288196C24F8a3580b3a21B240;
 
 
-    function Withdraw() external payable {
-        require(msg.sender == real_owner, "You're not the real owner");
-        uint256 balance = address(this).balance;
-        (bool transfer, ) = payable(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db).call{value: balance}("");
-        require(transfer, "Transfer failed");
+ //   function Withdraw() external payable {
+   //     require(msg.sender == real_owner, "You're not the real owner");
+   //     uint256 balance = address(this).balance;
+   //     (bool transfer, ) = payable(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db).call{value: balance}("");
+   //     require(transfer, "Transfer failed");
+    //}
+    modifier onlyOwner(){
+        require(msg.sender == owner ||real_owner , "Can only be called by the owner");
+        _;
     }
 
 }
